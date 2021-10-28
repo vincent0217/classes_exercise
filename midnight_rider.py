46cbe39f8a9a53a8568e84cd7a6e08fd16b74f08
Vince Zeng <vince.zeng0217@gmail.com>
2021-10-28 12:12:14 -0700

    provided player choices to end the game

--- midnight_rider.py
+++ midnight_rider.py
@@ -1,6 +1,5 @@
 # Midnight Rider
 
-
 import sys
 import textwrap
 import time
@@ -12,7 +11,12 @@ import midnight_rider_text
 class Game:
     """Represent our game engine
 
+    Attribute:
+        done: describes the game is finished or not - boolean
     """
+    def __init__(self):
+        self.done = False
+
     def introduction(self) -> None:
         """Print the introduction text"""
         print(midnight_rider_text.INTRODUCTION)
@@ -25,10 +29,31 @@ class Game:
             sys.stdout.write(char)
             sys.stdout.flush()
 
+    def show_choices(self) -> None:
+        """Show the user their choices"""
+        time.sleep(1)
+        print(midnight_rider_text.CHOICES)
+        time.sleep(1)
+
+    def get_choice(self) -> None:
+        """Gets the user's choice and changes the environment"""
+        # Get the user's response
+        user_choice = input().lower().strip("?!.,")
+
+        # Based on their choice, change the attributes of the class
+        if user_choice == "q":
+            self.done = True
+
 
 def main() -> None:
-    game = Game() # Starting a new game
+    game = Game()    # Starting a new game
     game.introduction()
+
+    while not game.done:
+        game.show_choices()
+        # TODO: Ask the player what they want to do
+        game.get_choice()
+        # TODO: Check win/lose condition
 
 
 if __name__ == "__main__":
--- midnight_rider_text.py
+++ midnight_rider_text.py
@@ -1,11 +1,20 @@
 # Constants
 
 INTRODUCTION = """Welcome to Midnight Rider
+
 WE'VE STOLEN A CAR. WE NEED TO GET IT HOME.
 THE CAR IS SPECIAL.
+
 THAT'S WHY THE GOVERNMENT WANTS IT.
+
 WE CAN'T LET THEM HAVE IT.
+
 ONE GOAL: SURVIVAL... AND THE CAR
 REACH THE SAFEHOUSE BEFORE THE MAN
 GON GETCHU.
 """
+
+CHOICES = """
+    --- Your Choices ---
+    Q.Quit the game
+    --- """
