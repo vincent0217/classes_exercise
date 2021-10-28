a986d714386551a92299fb542c228806f1dc95e2
Vince Zeng <vince.zeng0217@gmail.com>
2021-10-28 11:49:36 -0700

    Created method to print out character by character

--- midnight_rider.py
+++ midnight_rider.py
@@ -1,5 +1,9 @@
 # Midnight Rider
 
+
+import sys
+import textwrap
+import time
 import midnight_rider_text
 
 # A text-based game of intrigue and illusion
@@ -12,6 +16,14 @@ class Game:
     def introduction(self) -> None:
         """Print the introduction text"""
         print(midnight_rider_text.INTRODUCTION)
+        self.typewriter_effect(midnight_rider_text.INTRODUCTION)
+
+    def typewriter_effect(self, text: str) -> None:
+        """Print out to console with a typewriter effect"""
+        for char in textwrap.dedent(text):
+            time.sleep(0.05)
+            sys.stdout.write(char)
+            sys.stdout.flush()
 
 
 def main() -> None:
