c2d21bcdf978e4b83b05f3070e3946affa8b3442
Vince Zeng <vince.zeng0217@gmail.com>
2021-11-19 09:40:57 -0800

    Finalized last details.

--- relaxing_snowscape.py
+++ relaxing_snowscape.py
@@ -14,25 +14,27 @@ GREEN = (  0, 255,   0)
 BLUE  = (  0,   0, 255)
 BGCOLOUR = BLACK
 
-SCREEN_WIDTH  = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH  = 1024
+SCREEN_HEIGHT = 768
 SCREEN_SIZE   = (SCREEN_WIDTH, SCREEN_HEIGHT)
 WINDOW_TITLE  = "Relaxing Snowscape"
 
 
 class Snowflake:
     """Snowflake on the screen
-
     Attributes:
         size: radius of snowflake in px
-        coords: {x: int, y: int}
-        velocity: falling velocity in px/sec
-        colour: (r, b, g)
+        coords: {x: int, y:int}
+        y_vel: falling velocity in px/sec
+        colour: (r, g, b)
     """
     def __init__(self):
         self.size = 2
         # randomly place the snow on the screen
-        self.coords = [random.randrange(0, SCREEN_WIDTH), random.randrange(0, SCREEN_HEIGHT)]
+        self.coords = [
+            random.randrange(0, SCREEN_WIDTH),
+            random.randrange(0, SCREEN_HEIGHT)
+        ]
         self.y_vel = 4
         self.colour = WHITE
 
@@ -41,7 +43,7 @@ class Snowflake:
         # Changes the y portion of the coords
         self.coords[1] += self.y_vel
 
-        # Reset position of snowflake if it reaches the bottom
+        # Reset position of snowflake if it reaches bottom
         if self.coords[1] > SCREEN_HEIGHT:
             self.coords = [
                 random.randrange(0, SCREEN_WIDTH),
@@ -61,14 +63,19 @@ def main() -> None:
     num_snowflakes = 250
     snowflakes = []
     # Create snowflakes in foreground
-
+    for i in range(num_snowflakes-150):
+        close_snowflake = Snowflake()
+        close_snowflake.size = random.choice([4, 5, 6])
+        close_snowflake.y_vel = random.choice([1, 2])
+        snowflakes.append(close_snowflake)
+    for i in range(num_snowflakes-100):
+        close_snowflake = Snowflake()
+        close_snowflake.size = random.choice([3, 4])
+        close_snowflake.y_vel = random.choice([2, 3])
+        snowflakes.append(close_snowflake)
+    # Create snowflakes in background
     for i in range(num_snowflakes):
         snowflakes.append(Snowflake())
-    for i in range(num_snowflakes):
-        close_snowflake = Snowflake()
-        close_snowflake.size = 4
-        close_snowflake.y_vel = 3
-        snowflakes.append(close_snowflake)
 
 
     # ----------- MAIN LOOP
@@ -85,7 +92,9 @@ def main() -> None:
         # ----------- DRAW THE ENVIRONMENT
         screen.fill(BGCOLOUR)      # fill with bgcolor
 
-        # draw the snowflake
+        pygame.draw.rect(screen, (150, 150, 255), [0, 500, SCREEN_WIDTH, 300])
+
+        # draw the snowflakes
         for snow in snowflakes:
             pygame.draw.circle(screen, snow.colour, snow.coords, snow.size)
 
