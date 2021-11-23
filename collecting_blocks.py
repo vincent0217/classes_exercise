e39d11f50aa957f27faf55079138d3795295ee11
Vince Zeng <vince.zeng0217@gmail.com>
2021-11-23 11:32:43 -0800

    Finalized last details.

---
+++ collecting_blocks.py
@@ -0,0 +1,95 @@
+# Collecting Blocks
+# Author: Vincent
+# 2021 Version
+
+
+import pygame
+
+pygame.init()
+
+WHITE =     (255, 255, 255)
+BLACK =     (  0,   0,   0)
+RED   =     (255,   0,   0)
+GREEN =     (  0, 255,   0)
+BLUE  =     (  0,   0, 255)
+ETON_BLUE = (135, 187, 162)
+RAD_RED =   (255,  56, 100)
+BLK_CHOCOLATE = (25, 17, 2)
+
+BGCOLOUR =  WHITE
+
+SCREEN_WIDTH  = 800
+SCREEN_HEIGHT = 600
+SCREEN_SIZE   = (SCREEN_WIDTH, SCREEN_HEIGHT)
+WINDOW_TITLE  = "Collecing Blocks"
+
+
+class Block(pygame.sprite.Sprite):
+    """Describes a block object
+    A subclass of pygame.sprite.Sprite
+    Attributes:
+        image: Surface that is the visual
+            representation of our Block
+        rect: numerical representation of
+            our Block [x, y, width, height]
+    """
+    def __init__(self, colour: tuple, width: int, height: int) -> None:
+        """
+        Arguments:
+        :param colour: 3-tuple (r, g, b)
+        :param width: width in pixels
+        :param height: height in pixels
+        """
+        # Call the superclass constructor
+        super().__init__()
+
+        # Create the image of the block
+        self.image = pygame.Surface([width, height])
+        self.image.fill(colour)
+
+        # Based on the image, create a Rect for the block
+        self.rect = self.image.get_rect()
+
+def main() -> None:
+    """Driver of the Python script"""
+    # Create the screen
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption(WINDOW_TITLE)
+
+    # Create some local variables that describe the environment
+    done = False
+    clock = pygame.time.Clock()
+
+    # Create a group of sprites to store ALL SPRITES
+    all_sprites = pygame.sprite.Group()
+
+    # Create the Player block
+    player = Block(ETON_BLUE, 20, 15)
+    # Add the Player to all_sprites group
+    all_sprites.add(player)
+
+
+    # ----------- MAIN LOOP
+    while not done:
+        # ----------- EVENT LISTENER
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        # ----------- CHANGE ENVIRONMENT
+
+        # ----------- DRAW THE ENVIRONMENT
+        screen.fill(BGCOLOUR)      # fill with bgcolor
+
+        # Draw all sprites
+        all_sprites.draw(screen)
+
+        # Update the screen
+        pygame.display.flip()
+
+        # ----------- CLOCK TICK
+        clock.tick(75)
+
+
+if __name__ == "__main__":
+    main()
