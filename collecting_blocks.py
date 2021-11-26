1cee0d42be81f46893b7a408989c27159c6046d2
Vince Zeng <vince.zeng0217@gmail.com>
2021-11-26 09:50:17 -0800

    added enemies

--- collecting_blocks.py
+++ collecting_blocks.py
@@ -4,6 +4,7 @@
 
 import random
 import pygame
+import time
 
 pygame.init()
 
@@ -22,6 +23,27 @@ SCREEN_WIDTH  = 800
 SCREEN_HEIGHT = 600
 SCREEN_SIZE   = (SCREEN_WIDTH, SCREEN_HEIGHT)
 WINDOW_TITLE  = "Collecing Blocks"
+
+
+class Player(pygame.sprite.Sprite):
+    """Describes a player object
+    A subclass of pygame.sprite.Sprite
+    Attributes:
+        image: Surface that is the visual
+            representation of our Block
+        rect: numerical representation of
+            our Block [x, y, width, height]
+    """
+    def __init__(self) -> None:
+        # Call the superclass constructor
+        super().__init__()
+
+        # Create the image of the block
+        self.image = pygame.image.load("./images/smb_mario.png")
+        self.image = pygame.transform.scale(self.image, (48, 64))
+
+        # Based on the image, create a Rect for the block
+        self.rect = self.image.get_rect()
 
 
 class Block(pygame.sprite.Sprite):
@@ -51,6 +73,54 @@ class Block(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
 
 
+class Enemy(pygame.sprite.Sprite):
+    """The enemy sprites
+    Attributes:
+        image: Surface that is the visual representation
+        rect: Rect (x, y, width, height)
+        x_vel: x velocity
+        y_vel: y velocity
+    """
+    def __init__(self):
+        super().__init__()
+
+        self.image = pygame.image.load("./images/smb_goomba.png")
+        # Resize the image (scale)
+        self.image = pygame.transform.scale(self.image, (91, 109))
+
+        self.rect = self.image.get_rect()
+        # Define the initial location
+        self.rect.x, self.rect.y = (
+            random.randrange(SCREEN_WIDTH),
+            random.randrange(SCREEN_HEIGHT)
+        )
+
+        # Define the initial velocity
+        self.x_vel = random.choice([-4, -3, 3, 4])
+        self.y_vel = random.choice([-4, -3, 3, 4])
+
+    def update(self) -> None:
+        """Calculate movement"""
+        self.rect.x += self.x_vel
+        self.rect.y += self.y_vel
+
+        # Constrain movement
+        # X -
+        if self.rect.left < 0:
+            self.rect.x = 0
+            self.x_vel = -self.x_vel    # bounce
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+            self.x_vel = -self.x_vel    # bounce
+        # Y -
+        if self.rect.y < 0:
+            self.rect.y = 0
+            self.y_vel = -self.y_vel
+        if self.rect.bottom > SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+            self.y_vel = -self.y_vel
+
+
 def main() -> None:
     """Driver of the Python script"""
     # Create the screen
@@ -61,14 +131,18 @@ def main() -> None:
     done = False
     clock = pygame.time.Clock()
     num_blocks = 100
+    num_enemies = 10
+    score = 0
+    start_time = time.time()
 
     pygame.mouse.set_visible(False)
 
-    # Create groups the hold sprites
+    # Create groups to hold Sprites
     all_sprites = pygame.sprite.Group()
     block_sprites = pygame.sprite.Group()
+    enemy_sprites = pygame.sprite.Group()
 
-    # Create all the block sprites and add to the block_sprites
+    # Create all the block sprites and add to block_sprites
     for i in range(num_blocks):
         # Create a block (set its parameters)
         block = Block(BLACK, 20, 15)
@@ -76,14 +150,23 @@ def main() -> None:
         # Set a random location for the block inside the screen
         block.rect.x = random.randrange(SCREEN_WIDTH - block.rect.width)
         block.rect.y = random.randrange(SCREEN_HEIGHT - block.rect.height)
+
         # Add the block to the block_sprites Group
-        # ADd the block to the all_sprites Group
+        # Add the block to the all_sprites Group
         block_sprites.add(block)
         all_sprites.add(block)
 
+    # Create enemy sprites
+    for i in range(num_enemies):
+        # Create an enemy
+        enemy = Enemy()
+
+        # Add it to the sprites list (enemy_sprites and all_sprites)
+        enemy_sprites.add(enemy)
+        all_sprites.add(enemy)
 
     # Create the Player block
-    player = Block(ETON_BLUE, 20, 15)
+    player = Player()
     # Add the Player to all_sprites group
     all_sprites.add(player)
 
@@ -99,10 +182,18 @@ def main() -> None:
 
         # ----------- CHANGE ENVIRONMENT
         # Process player movement based on mouse pos
-
         mouse_pos = pygame.mouse.get_pos()
-        player.rect = mouse_pos
+        player.rect.x, player.rect.y = mouse_pos
 
+        # Update the location of all sprites
+        all_sprites.update()
+
+        # Check all collisions between player and the blocks
+        blocks_collided = pygame.sprite.spritecollide(player, block_sprites, True)
+
+        for block in blocks_collided:
+            score += 1
+            print(f"Score: {score}")
         # ----------- DRAW THE ENVIRONMENT
         screen.fill(BGCOLOUR)      # fill with bgcolor
 
@@ -114,6 +205,9 @@ def main() -> None:
 
         # ----------- CLOCK TICK
         clock.tick(75)
+
+    total_time = time.time() - start_time
+    print(f"Time: {total_time}")
 
 
 if __name__ == "__main__":
